e7890dde4864e61cd5635cf89fc786d91c403429
dragonflypie <canuczerealme@gmail.com>
2022-04-25 12:45:05 +0300

    Make css responsive
    styling:
    - Adjust sizings
    - Implement media queries
    - Introduce color scheme

--- src/components/hourly/Hourly.tsx
+++ src/components/hourly/Hourly.tsx
@@ -17,6 +17,22 @@ const Hourly = () => {
     infinite: false,
     slidesToShow: 5,
     slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 728,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
